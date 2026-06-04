68b6226a543ec7d4a29f474648d345e7a7863d70
Gabriel Bourget <gabrielbourget@gmail.com>
2026-06-04 15:25:45 -0400

    Add Modal Source Receipt Proof

---
+++ packages/react/src/registry/modal-ingest-packet.ts
@@ -0,0 +1,4 @@
+import type { TRegistryIngestPacket } from "./ingest"
+import modalIngestPacketData from "./modal-ingest-packet.data.json"
+
+export const modalIngestPacket = modalIngestPacketData as TRegistryIngestPacket
